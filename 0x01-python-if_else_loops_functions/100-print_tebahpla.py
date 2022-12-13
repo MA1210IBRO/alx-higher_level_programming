36d73c6781ae956400fe09337031456a4c2e6e3e
MA1210IBRO <106749780+MA1210IBRO@users.noreply.github.com>
2022-12-13 05:22:06 -0800

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 # Author Bamidele Adefolaju
-
-=======
-# Author -Bamidele Adefolaju
->>>>>>> 335b802700caa4b199fc44eb6a8f6eaddcfbf378
 i = 0
 for c in range(ord('z'). ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
     i = 32 if i == 0 else 0
-
