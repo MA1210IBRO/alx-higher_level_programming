6eace93a1d1ccd8d4cd63042a4434e49b99fbde8
MA121010IBRO <email4mame@gmail.com>
2022-12-20 04:28:12 -0800

    11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,17 @@
+#include <python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(pyobject *p)
+{
+        long int size = pyList_size(p);
+        int i;
+        pyListobject *obj = (pyListobject *)p;
+
+        printf("[*] size of the python list = %li\n", size);
+        printf("[*] Allocated = %li\n", obj->allocated);
+        for (i = 0; i < size; i++)
+                printf("Element %i: %s\n", i, py_TYPE(obj->ob_item[i])->tp_name);
+}
+
+
